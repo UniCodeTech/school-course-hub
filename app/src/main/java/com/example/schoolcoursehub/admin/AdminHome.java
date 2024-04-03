72e540e39e1415f80bc988618f1e384f765d1884
Dinith Maleesha <maleesha.live123@gmail.com>
2024-04-03 19:45:10 +0530

    add Admin Menu

--- app/src/main/java/com/example/schoolcoursehub/admin/AdminHome.java
+++ app/src/main/java/com/example/schoolcoursehub/admin/AdminHome.java
@@ -1,8 +1,11 @@
 package com.example.schoolcoursehub.admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.schoolcoursehub.R;
 
@@ -14,4 +17,34 @@ public class AdminHome extends AppCompatActivity {
         setContentView(R.layout.activity_admin_home);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.admin_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle item selection
+        if(item.getItemId()==R.id.new_course){
+            addNewCourse();
+        } else if(item.getItemId()==R.id.view_user){
+            viewUserList();
+        } else if (item.getItemId()==R.id.add_branch) {
+            addBranch();
+        }
+        return true;
+    }
+
+
+    // TODO: implement below functions
+    private void addNewCourse() {
+
+    }
+
+    private void viewUserList() {
+    }
+
+    private void addBranch() {
+    }
 }
