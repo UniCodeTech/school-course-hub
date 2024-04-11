fa2e3a1d022581659207a8922efe61b534789b1e
Dinith Maleesha <maleesha.live123@gmail.com>
2024-04-11 10:52:23 +0530

    Add Course card view to home screen

--- app/src/main/java/com/example/schoolcoursehub/MainActivity.java
+++ app/src/main/java/com/example/schoolcoursehub/MainActivity.java
@@ -6,14 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.schoolcoursehub.admin.AdminHome;
+import com.example.schoolcoursehub.admin.UpdateCourseActivity;
+import com.example.schoolcoursehub.helper.Course;
 import com.example.schoolcoursehub.helper.DBHandler;
 import com.example.schoolcoursehub.signupandlogin.LoginActivity;
 import com.example.schoolcoursehub.signupandlogin.RegistrationActivity;
 
 import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,14 +38,45 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Database", "Database does not exist");
         }
 
-        Intent intent = new Intent(MainActivity.this, AdminHome.class);
-        intent.putExtra("userId", 1);
-        startActivity(intent);
+        displayCourses();
 
     }
 
+    private void displayCourses() {
+        List<Course> courses = db.getAllCourses();
+
+        LinearLayout courseListLayout = findViewById(R.id.courseListLayout);
+        courseListLayout.removeAllViews();
+
+        for (Course course : courses) {
+            View courseCardView = getLayoutInflater().inflate(R.layout.item_course, null);
+
+            TextView courseNameTextView = courseCardView.findViewById(R.id.course_name);
+            TextView courseCostTextView = courseCardView.findViewById(R.id.course_cost);
+            TextView coursDurationTextView = courseCardView.findViewById(R.id.course_duration);
+            TextView maxParticipantTextView = courseCardView.findViewById(R.id.max_participant);
+            TextView startDateTextView = courseCardView.findViewById(R.id.start_date);
+
+            courseNameTextView.setText(course.getCourseName());
+            courseCostTextView.setText(String.valueOf(course.getCourseCost()));
+            coursDurationTextView.setText(String.valueOf(course.getCourseDuration()));
+            maxParticipantTextView.setText(String.valueOf(course.getMaxParticipants()));
+            startDateTextView.setText(String.valueOf(course.getStartingDate()));
+
+            final int courseId = course.getCourseId();
+            courseCardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Course Name: "+course.getCourseName(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            courseListLayout.addView(courseCardView);
+        }
+    }
+
     public void onRegisterButtonClick(View view) {
-        // Handle register button click
+
         System.out.println("Register Button Clicked");
 
         Intent intent = new Intent(this, RegistrationActivity.class);
@@ -48,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLoginButtonClick(View view) {
-        // Handle login button click
+        
         System.out.println("Login Button Clicked");
 
         Intent intent = new Intent(this, LoginActivity.class);
--- app/src/main/java/com/example/schoolcoursehub/admin/AdminHome.java
+++ app/src/main/java/com/example/schoolcoursehub/admin/AdminHome.java
@@ -49,14 +49,16 @@ public class AdminHome extends AppCompatActivity {
             View courseCardView = getLayoutInflater().inflate(R.layout.item_course, null);
 
             TextView courseNameTextView = courseCardView.findViewById(R.id.course_name);
+            TextView courseCostTextView = courseCardView.findViewById(R.id.course_cost);
             TextView coursDurationTextView = courseCardView.findViewById(R.id.course_duration);
             TextView maxParticipantTextView = courseCardView.findViewById(R.id.max_participant);
             TextView startDateTextView = courseCardView.findViewById(R.id.start_date);
 
             courseNameTextView.setText(course.getCourseName());
-            coursDurationTextView.setText("Duration: " + String.valueOf(course.getCourseDuration()));
-            maxParticipantTextView.setText("Max Participant: " + String.valueOf(course.getMaxParticipants()));
-            startDateTextView.setText("Start Date: " + String.valueOf(course.getStartingDate()));
+            courseCostTextView.setText(String.valueOf(course.getCourseCost()));
+            coursDurationTextView.setText(String.valueOf(course.getCourseDuration()));
+            maxParticipantTextView.setText(String.valueOf(course.getMaxParticipants()));
+            startDateTextView.setText(String.valueOf(course.getStartingDate()));
 
 
             // Set click listener for the card
